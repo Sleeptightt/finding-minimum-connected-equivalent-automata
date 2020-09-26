7355308897d9e09882176b4b7581c340e0f03f58
Carlos Restrepo <cr461204@gmail.com>
2020-09-25 22:19:31 -0500

    created interface, main controller and Main class

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,5 +1,26 @@
 package ui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+	
+	public static void main(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("guiAutomata.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		stage.setTitle("");
+		stage.setScene(scene);
+		stage.show();
+	}
+	
 }
---
+++ src/ui/MainController.java
@@ -0,0 +1,42 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class MainController {
+
+    @FXML
+    private TextField initialState;
+
+    @FXML
+    private TextField states;
+
+    @FXML
+    private TextField inputAlphabet;
+
+    @FXML
+    private TextField outputAlphabet;
+
+    @FXML
+    private ChoiceBox<String> typeAutomata;
+
+    @FXML
+    private Button btnConfirm;
+
+    @FXML
+    private TableView<?> tableAutomata;
+
+    @FXML
+    private void initialize() {
+    	typeAutomata.getItems().addAll("Mealy", "Moore");
+    }
+    @FXML
+    void fillTable(ActionEvent event) {
+
+    }
+
+}
