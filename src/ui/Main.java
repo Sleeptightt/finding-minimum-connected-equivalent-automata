6a23da6110c54c7bfcf280e9714c0c2210bfb0dd
Carlos Restrepo <cr461204@gmail.com>
2020-09-26 13:20:11 -0500

    functionality of create mealy and moore board has been done

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch();
-		test();
+		//test();
 	}
 
 	@Override
--- src/ui/MainController.java
+++ src/ui/MainController.java
@@ -1,11 +1,27 @@
 package ui;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.NodeOrientation;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+import model.MealyMachine;
+import model.MooreMachine;
 
 public class MainController {
 
@@ -13,7 +29,7 @@ public class MainController {
     private TextField initialState;
 
     @FXML
-    private TextField states;
+    private TextField statesField;
 
     @FXML
     private TextField inputAlphabet;
@@ -22,21 +38,114 @@ public class MainController {
     private TextField outputAlphabet;
 
     @FXML
-    private ChoiceBox<String> typeAutomata;
+    private ComboBox<String> typeAutomata;
 
     @FXML
     private Button btnConfirm;
 
     @FXML
-    private TableView<?> tableAutomata;
+    private ScrollPane scrollTable;
+    
+    private MealyMachine mealy;
+    private MooreMachine moore;
 
     @FXML
     private void initialize() {
+    	typeAutomata.getItems().addAll("Mealy", "Moore");
     	
-    }
-    @FXML
-    void fillTable(ActionEvent event) {
 
     }
+    @FXML
+    void generateTable(ActionEvent event) {
+    		String[] states = statesField.getText().split(",");
+    		String[] input = inputAlphabet.getText().split(",");
+    		String[] output = outputAlphabet.getText().split(",");
+    		String start= initialState.getText();
+    		String type = typeAutomata.getValue();
+    		
+    		if(type.equals("Mealy")) {
+    			generateMealyTable(input,states);
+    			mealy = new MealyMachine(states.length, input.length,output.length,0);
+    		}else {
+    			generateMooreTable(input,states);
+    			moore = new MooreMachine(states.length, input.length,output.length,0);
+    		}
+    		
+    }
+    
+    @FXML
+    void find_automata(ActionEvent event) {
+
+    }
+    
+    
+   public void generateMealyTable(String[] input, String[] states) {
+	   int length = states.length;
+	   int width = input.length;
+
+	   GridPane root = new GridPane();
+	   for(int y = 0; y < length+1; y++)
+	   {
+		   for(int x = 0; x < width+1; x++)
+		   {	
+	        	if(x==0 && y==0) {
+	        		Button a = new Button("states"); 
+	        		a.setPrefWidth(100);
+	                root.add(a, x,y);
+	        	}
+	        	else if(x==0 && y>0) {
+	        		Button a = new Button(states[y-1]); 
+	        		a.setPrefWidth(100);
+	                root.add(a, x,y);
+	        	}else if(y==0 && x>0) {
+	        		Button symbol = new Button(input[x-1]); 
+	        		symbol.setPrefWidth(100);
+	                root.add(symbol, x,y);
+	        	}else {
+	        		TextField a = new TextField("");
+	        		a.setPrefWidth(100);
+	        		root.add(a, x,y);
+	        	}
+		   }
+	   }   
+		scrollTable.setContent(root);
+   }
+
+   public void generateMooreTable(String[] input, String[] states) {
+	   int length = states.length;
+	   int width = input.length;
+
+	   GridPane root = new GridPane();
+	   for(int y = 0; y < length+1; y++)
+	   {
+		   for(int x = 0; x < width+2; x++)
+		   {	
+	        	if(x==0 && y==0) {
+	        		Button a = new Button("States"); 
+	        		a.setPrefWidth(100);
+	                root.add(a, x,y);
+	        	}else if(x==width+1 && y==0) {
+	        		Button a = new Button("Output"); 
+	        		a.setPrefWidth(100);
+	                root.add(a, x,y);
+	        	}
+	        	else if(x==0 && y>0) {
+	        		Button a = new Button(states[y-1]); 
+	        		a.setPrefWidth(100);
+	                root.add(a, x,y);
+	        	}else if(y==0 && x>0) {
+	        		Button symbol = new Button(input[x-1]); 
+	        		symbol.setPrefWidth(100);
+	                root.add(symbol, x,y);
+	        	}else {
+	        		TextField a = new TextField("");
+	        		a.setPrefWidth(100);
+	        		root.add(a, x,y);
+	        	}
+		   }
+	   }  
+	   scrollTable.setContent(root); 
+	 
+   }
 
 }
